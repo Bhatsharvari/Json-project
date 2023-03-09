112881a9996f60237c7d7d016e1b6f55cf131a34
Bhatsharvari <123368186+Bhatsharvari@users.noreply.github.com>
2023-03-10 00:14:26 +0530

    Add files via upload
    Added

---
+++ Korean.jsx
@@ -0,0 +1,135 @@
+import React from "react"
+
+const Korean=(props)=>{
+    return(
+        <div className="parentBlock" > 
+        
+
+        <div className="childBlock">
+                <img src={props.payload[0].CoverImage} alt="" />
+                <h2>{props.payload[0].DramaName}</h2>
+                <h3>{props.payload[0].Genres}</h3>
+                <h4>{props.payload[0].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[1].CoverImage} alt="" />
+                <h2>{props.payload[1].DramaName}</h2>
+                <h3>{props.payload[1].Genres}</h3>
+                <h4>{props.payload[1].ReleaseDate}</h4>
+                </div>
+               
+                <div className="childBlock">
+                <img src={props.payload[2].CoverImage} alt="" />
+                <h2>{props.payload[2].DramaName}</h2>
+                <h3>{props.payload[2].Genres}</h3>
+                <h4>{props.payload[2].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[3].CoverImage} alt="" />
+                <h2>{props.payload[3].DramaName}</h2>
+                <h3>{props.payload[3].Genres}</h3>
+                <h4>{props.payload[3].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[4].CoverImage} alt="" />
+                <h2>{props.payload[4].DramaName}</h2>
+                <h3>{props.payload[4].Genres}</h3>
+                <h4>{props.payload[4].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[5].CoverImage} alt="" />
+                <h2>{props.payload[5].DramaName}</h2>
+                <h3>{props.payload[5].Genres}</h3>
+                <h4>{props.payload[5].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[6].CoverImage} alt="" />
+                <h2>{props.payload[6].DramaName}</h2>
+                <h3>{props.payload[6].Genres}</h3>
+                <h4>{props.payload[6].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[7].CoverImage} alt="" />
+                <h2>{props.payload[7].DramaName}</h2>
+                <h3>{props.payload[7].Genres}</h3>
+                <h4>{props.payload[7].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[8].CoverImage} alt="" />
+                <h2>{props.payload[8].DramaName}</h2>
+                <h3>{props.payload[8].Genres}</h3>
+                <h4>{props.payload[8].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[9].CoverImage} alt="" />
+                <h2>{props.payload[9].DramaName}</h2>
+                <h3>{props.payload[9].Genres}</h3>
+                <h4>{props.payload[9].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[10].CoverImage} alt="" />
+                <h2>{props.payload[10].DramaName}</h2>
+                <h3>{props.payload[10].Genres}</h3>
+                <h4>{props.payload[10].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[11].CoverImage} alt="" />
+                <h2>{props.payload[11].DramaName}</h2>
+                <h3>{props.payload[11].Genres}</h3>
+                <h4>{props.payload[11].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[12].CoverImage} alt="" />
+                <h2>{props.payload[12].DramaName}</h2>
+                <h3>{props.payload[12].Genres}</h3>
+                <h4>{props.payload[12].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+               <img src={props.payload[13].CoverImage} alt="" />
+                <h2>{props.payload[13].DramaName}</h2>
+                <h3>{props.payload[13].Genres}</h3>
+                <h4>{props.payload[13].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[14].CoverImage} alt="" />
+                <h2>{props.payload[14].DramaName}</h2>
+                <h3>{props.payload[14].Genres}</h3>
+                <h4>{props.payload[14].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[15].CoverImage} alt="" />
+                <h2>{props.payload[15].DramaName}</h2>
+                <h3>{props.payload[15].Genres}</h3>
+                <h4>{props.payload[15].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[16].CoverImage} alt="" />
+                <h2>{props.payload[16].DramaName}</h2>
+                <h3>{props.payload[16].Genres}</h3>
+                <h4>{props.payload[16].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[17].CoverImage} alt="" />
+                <h2>{props.payload[17].DramaName}</h2>
+                <h3>{props.payload[17].Genres}</h3>
+                <h4>{props.payload[17].ReleaseDate}</h4>
+                </div>
+                <div className="childBlock">
+                <img src={props.payload[18].CoverImage} alt="" />
+                <h2>{props.payload[18].DramaName}</h2>
+                <h3>{props.payload[18].Genres}</h3>
+                <h4>{props.payload[18].ReleaseDate}</h4>
+                </div>
+
+                <div className="childBlock">
+                <img src={props.payload[19].CoverImage} alt="" />
+                <h2>{props.payload[19].DramaName}</h2>
+                <h3>{props.payload[19].Genres}</h3>
+                <h4>{props.payload[19].ReleaseDate}</h4>
+                </div>
+                
+
+            </div>
+    )
+}
+export default Korean
